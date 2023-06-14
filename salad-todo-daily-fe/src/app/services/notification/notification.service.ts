f96a369dc95446faa707fc865bae3438240ef359
Lê Đức Huy <leduchuy1461@gmail.com>
2023-06-14 23:31:20 +0700

    Thêm send notification service

---
+++ salad-todo-daily-fe/src/app/services/notification/notification.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  baseUrl: string = "https://api.telegram.org/bot5898997604:AAEMTODc64xcrLeP1yBMAXOJbt6ziaJmIEc/";
+  constructor(private http: HttpClient) { }
+
+  sendMessage(chatId: any, message: string) {
+    var sendMessageUrl = `${this.baseUrl}sendMessage?chat_id=${chatId}&text=${message}`;
+  }
+}
