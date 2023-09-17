4130e212c73bd7bb2c3fc10b404a58b0e156ca5a
Lê Đức Huy <leduchuy1461@gmail.com>
2023-09-17 23:07:51 +0700

    Sửa lỗi bị loading khi vào trang đăng nhập

--- salad-todo-daily-fe/src/app/routes/auth/login-screen/login-screen.component.ts
+++ salad-todo-daily-fe/src/app/routes/auth/login-screen/login-screen.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { LoadingService } from 'src/app/services/common/loading/loading.service';
 import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
-  styleUrls: ['./login-screen.component.css']
+  styleUrls: ['./login-screen.component.css'],
 })
 export class LoginScreenComponent implements OnInit {
   loginForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private msg: NzMessageService, private router: Router) { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private msg: NzMessageService,
+    private router: Router,
+    private loadingService: LoadingService
+  ) {
+    this.loadingService.setLoading(false);
+  }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -22,14 +31,16 @@ export class LoginScreenComponent implements OnInit {
     });
   }
 
-  login(){
-    this.authService.login(this.loginForm.value).toPromise().then((res) => {
-      if(!res.status) {
-        this.msg.error("Tài khoản hoặc mật khẩu không chính xác!");
-      } else {
-        this.msg.success("Đăng nhập thành công");
-      }
-    });
+  login() {
+    this.authService
+      .login(this.loginForm.value)
+      .toPromise()
+      .then((res) => {
+        if (!res.status) {
+          this.msg.error('Tài khoản hoặc mật khẩu không chính xác!');
+        } else {
+          this.msg.success('Đăng nhập thành công');
+        }
+      });
   }
-
 }
