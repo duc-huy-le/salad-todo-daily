72f3a412b19c887b1748cd0f828f804c5827317b
Lê Đức Huy <leduchuy1461@gmail.com>
2023-10-22 09:51:19 +0700

    Refactor code BE Project model và controller

--- salad-todo-daily-be/app/controllers/project.controller.js
+++ salad-todo-daily-be/app/controllers/project.controller.js
@@ -1,61 +1,88 @@
 var Project = require("../models/project.model");
 var JWT = require("../common/_JWT");
-const {
-  getFormattedMySqlDateTime,
-} = require("../helpers/helper");
+const { getFormattedMySqlDateTime } = require("../helpers/helper");
 
+const handleRequest = async (req, res, callback) => {
+  try {
+    const token = req.headers.authorization;
+    const tokenInfo = await JWT.check(token);
+    await callback(tokenInfo.data.id, req, res);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+};
+// exports.getList = async function (req, res) {
+//   try {
+//     const token = req.headers.authorization;
+//     const tokenInfo = await JWT.check(token);
+//     const projects = await Project.getAll(tokenInfo.data.id);
+//     res.send({ result: projects });
+//   } catch (err) {
+//     res.status(500).send({ error: err.message });
+//   }
+// };
 exports.getList = async function (req, res) {
-  const token = req.headers.authorization;
-  const tokenInfo = await JWT.check(token);
-  Project.getAll(tokenInfo.data.id, function (data) {
-    res.send({ result: data });
+  handleRequest(req, res, async (userId) => {
+    const projects = await Project.getAll(userId);
+    res.send({ result: projects });
   });
 };
 
 exports.getById = async function (req, res) {
-  const token = req.headers.authorization;
-  const tokenInfo = await JWT.check(token);
-  Project.getById(tokenInfo.data.id, req.params.id, function (response) {
-    res.send({ result: response });
+  handleRequest(req, res, async (userId) => {
+    const project = await Project.getById(userId, req.params.id);
+    res.send({ result: project });
   });
 };
 
 exports.add = async function (req, res) {
-  var data = req.body;
-  const token = req.headers.authorization;
-  const tokenInfo = await JWT.check(token);
-  data.createdBy = tokenInfo.data.id;
-  if (data.startDate)
-    data.startDate = getFormattedMySqlDateTime(data.startDate);
-
-  if (data.finishDate)
-    data.finishDate = getFormattedMySqlDateTime(data.finishDate);  
-  Project.create(data, function (response) {
-    res.send({ result: response });
+  handleRequest(req, res, async (userId) => {
+    const data = req.body;
+    data.createdBy = userId;
+    if (data.startDate)
+      data.startDate = getFormattedMySqlDateTime(data.startDate);
+    if (data.finishDate)
+      data.finishDate = getFormattedMySqlDateTime(data.finishDate);
+    const project = await Project.create(data);
+    res.send({ result: project });
   });
 };
 
-exports.update = function (req, res) {
-  var data = req.body;
-  if (data.startDate)
-    data.startDate = getFormattedMySqlDateTime(data.startDate);
-  if (data.finishDate)
-    data.finishDate = getFormattedMySqlDateTime(data.finishDate);
-  Project.update(req.params.id, data, function (response) {
-    res.send({ result: response });
+exports.update = async function (req, res) {
+  handleRequest(req, res, async (userId) => {
+    const data = req.body;
+    if (data.startDate)
+      data.startDate = getFormattedMySqlDateTime(data.startDate);
+    if (data.finishDate)
+      data.finishDate = getFormattedMySqlDateTime(data.finishDate);
+    const updateResult = await Project.update(req.params.id, data);
+    if (updateResult.affectedRows > 0) {
+      const project = await Project.getById(userId, req.params.id);
+      res.send({ result: project });
+    }
   });
 };
 
-exports.updateLittle = function (req, res) {
-  var data = req.body;
-  Project.updateLittle(req.params.id, data, function (response) {
-    res.send({ result: response });
+exports.updateLittle = async function (req, res) {
+  handleRequest(req, res, async (userId) => {
+    const data = req.body;
+    if (data.startDate)
+      data.startDate = getFormattedMySqlDateTime(data.startDate);
+    if (data.finishDate)
+      data.finishDate = getFormattedMySqlDateTime(data.finishDate);
+    const updateResult = await Project.update(req.params.id, data);
+    if (updateResult.affectedRows > 0) {
+      const project = await Project.getById(userId, req.params.id);
+      res.send({ result: project });
+    }
   });
 };
-
-exports.remove = function (req, res) {
-  var id = req.params.id;
-  Project.remove(id, function (response) {
-    res.send({ result: response });
+exports.remove = async function (req, res) {
+  handleRequest(req, res, async (userId) => {
+    const project = await Project.getById(userId, req.params.id);
+    const removeResult = await Project.remove(req.params.id);
+    if (removeResult.affectedRows > 0) {
+      res.send({ result: project });
+    }
   });
 };
--- salad-todo-daily-be/app/models/project.model.js
+++ salad-todo-daily-be/app/models/project.model.js
@@ -1,116 +1,133 @@
 const db = require("../common/connect");
-const Project = function (project) {
-  this.id = project.id;
-  this.name = project.name;
-  this.description = project.description;
-  this.color = project.color;
-  this.totalTask = project.totalTask;
-  this.completedTask = project.completedTask;
-  this.isDeleted = project.isDeleted;
-  this.startDate = project.startDate;
-  this.createdAt = project.createdAt;
-};
 
 const tableName = "project";
 
-Project.getAll = function (userId, result) {
-  db.query(
-    `select * from ${tableName} where createdBy = ${userId} and isDeleted = 0`,
-    function (err, data) {
-      if (err) {
-        result(null);
-      } else {
-        result(data);
-      }
-    }
-  );
-};
-
-Project.getById = function (userId, projectId, result) {
-  db.query(
-    `select * from ${tableName} where id = ${projectId} and createdBy = ${userId}`,
-    function (err, data) {
-      if (err || data.length == 0) {
-        result(null);
-      } else result(data);
-    }
-  );
-};
-
-Project.create = function (payload, result) {
-  db.query(`insert into ${tableName} set ?`, payload, function (err, data) {
-    if (err) {
-      result(null);
-    } else result({ id: data.insertId, ...payload });
-  });
-};
-
-Project.update = function (recordId, payload, result) {
-  db.query(
-    `update ${tableName} set name = ?, description = ?, color = ?, isDeleted = ?, startDate = ?, createdAt = ? where id = ${recordId}`,
-    [
-      payload.name,
-      payload.description,
-      payload.color,
-      payload.isDeleted,
-      payload.startDate,
-      payload.createdAt,
-    ],
-    function (err, data) {
-      if (err) {
-        result(err);
-      } else {
-        db.query(
-          `select * from ${tableName} where id = ${recordId}`,
-          function (err, updatedData) {
-            if (err) {
-              result(err);
-            } else {
-              result(updatedData);
-            }
-          }
-        );
-      }
-    }
-  );
-};
-
-Project.updateLittle = function (recordId, payload, result) {
-  let query = `update ${tableName} set`;
-  const fields = Object.keys(payload);
-  const fieldValues = [];
-  for (let i = 0; i < fields.length; i++) {
-    if (fields[i] !== "id") {
-      query += ` ${fields[i]} = ?,`;
-      fieldValues.push(payload[fields[i]]);
-    }
+class Project {
+  constructor(project) {
+    this.id = project.id;
+    this.name = project.name;
+    this.description = project.description;
+    this.color = project.color;
+    this.totalTask = project.totalTask;
+    this.completedTask = project.completedTask;
+    this.isDeleted = project.isDeleted;
+    this.startDate = project.startDate;
+    this.createdAt = project.createdAt;
   }
-  query = query.slice(0, -1);
-  query += ` where id = ${recordId}`;
-  db.query(query, fieldValues, function (err, data) {
-    if (err) {
-      result(err);
-    } else {
+
+  static getAll(userId) {
+    return new Promise((resolve, reject) => {
+      const querySQL = `SELECT * FROM ${tableName} WHERE createdBy = ${userId} AND isDeleted = 0`;
+      db.query(querySQL, function (err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  static getById(userId, projectId) {
+    return new Promise((resolve, reject) => {
+      const querySQL = `SELECT * FROM ${tableName} WHERE createdBy = ${userId} AND id = ${projectId}`;
+      db.query(querySQL, function (err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  static create(payload) {
+    return new Promise((resolve, reject) => {
+      db.query(`INSERT INTO ${tableName} SET ?`, payload, function (err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ id: data.insertId, ...payload });
+        }
+      });
+    });
+  }
+
+  static update(recordId, payload) {
+    return new Promise((resolve, reject) => {
       db.query(
-        `select * from ${tableName} where id = ${recordId}`,
-        function (err, updatedData) {
+        `UPDATE ${tableName} SET ? WHERE id = ${recordId}`,
+        payload,
+        function (err, data) {
           if (err) {
-            result(err);
+            reject(err);
           } else {
-            result(updatedData);
+            resolve(data);
           }
         }
       );
-    }
-  });
-};
+    });
+  }
 
-Project.remove = function (id, result) {
-  db.query(`delete from project where id = ${id}`, function (err, project) {
-    if (err) {
-      result(null);
-    } else result("Xóa user co id: " + id + " thành công");
-  });
-};
+  static updateLittle(recordId, payload) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `UPDATE ${tableName} SET ? WHERE id = ${recordId}`,
+        payload,
+        function (err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        }
+      );
+    });
+  }
+  static remove(recordId) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `DELETE FROM ${tableName} WHERE id = ${recordId}`,
+        function (err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        }
+      );
+    });
+  }
+}
 
 module.exports = Project;
+
+// Project.updateLittle = function (recordId, payload, result) {
+//   let query = `update ${tableName} set`;
+//   const fields = Object.keys(payload);
+//   const fieldValues = [];
+//   for (let i = 0; i < fields.length; i++) {
+//     if (fields[i] !== "id") {
+//       query += ` ${fields[i]} = ?,`;
+//       fieldValues.push(payload[fields[i]]);
+//     }
+//   }
+//   query = query.slice(0, -1);
+//   query += ` where id = ${recordId}`;
+//   db.query(query, fieldValues, function (err, data) {
+//     if (err) {
+//       result(err);
+//     } else {
+//       db.query(
+//         `select * from ${tableName} where id = ${recordId}`,
+//         function (err, updatedData) {
+//           if (err) {
+//             result(err);
+//           } else {
+//             result(updatedData);
+//           }
+//         }
+//       );
+//     }
+//   });
+// };
